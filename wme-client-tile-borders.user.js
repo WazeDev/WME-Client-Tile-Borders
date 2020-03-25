49eebec5f2f57b3bc4799edbc8da231982580426
Joyriding <39777209+Joyriding@users.noreply.github.com>
2020-03-25 16:36:20 -0400

    Update wme-client-tile-borders.user.js
    Bumped version to force WazeWrap update.

--- wme-client-tile-borders.user.js
+++ wme-client-tile-borders.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Client Tile Borders
 // @namespace    https://greasyfork.org/en/users/32336-joyriding
-// @version      1.6
+// @version      1.7
 // @description  Displays grid lines representing tile borders in the client.
 // @author       Joyriding
 // @include      /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
